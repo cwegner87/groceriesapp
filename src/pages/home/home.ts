6bc6f1eb9fe27435fe600a82a8176921c2166e4e
cwegner87 <cwegner1@live.maryville.edu>
2022-08-05 16:56:57 -0400

    adding items and update icon to cart

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,76 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title = "Grocery";
 
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Bread",
+      quantity: 1
+    },
+    {
+      name: "Banana",
+      quantity: 3
+    },
+    {
+      name: "Sugar",
+      quantity: 1
+    },
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  removeItem(item) {
+    console.log("Removing Item - ", item);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
