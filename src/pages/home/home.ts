27aa8808f3739bb9f630bb8997e1ce6b854d2abf
cwegner87 <cwegner1@live.maryville.edu>
2022-08-07 17:41:36 -0400

    adding data service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
 
 
 @Component({
@@ -12,74 +14,40 @@ export class HomePage {
 
   title = "Grocery";
 
-  items = [
-    {
-      name: "Milk",
-      quantity: 1
-    },
-    {
-      name: "Soda",
-      quantity: 1
-    },
-    {
-      name: "Banana",
-      quantity: 3
-    },
-    {
-      name: "Steak",
-      quantity: 1
-    },
-  ];
-
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public inputDialogService: InputDialogServiceProvider) {
 
   }
 
-  removeItem(item) {
-    console.log("Removing Item - ", item);
+  loadItems() {
+    return this.dataService.getItems();
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
     const toast = this.toastCtrl.create({
-      message: 'Removing Item - ' + item.name + " ...",
+      message: 'Removing Item - ' + index + " ...",
       duration: 3000
     });
     toast.present();
+
+    this.dataService.removeItem(index);
+  }
+
+  editItem(item, index) {
+    console.log("Edit Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Editing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+    this.inputDialogService.showPrompt(item, index);
   }
 
   addItem() {
     console.log("Adding Item");
-    this.showAddItemPrompt();
+    this.inputDialogService.showPrompt();
   }
 
-  showAddItemPrompt() {
-    const prompt = this.alertCtrl.create({
-      title: 'Add Item',
-      message: "Please enter item...",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Name'
-        },
-        {
-          name: 'quantity',
-          placeholder: 'Quantity'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Save',
-          handler: item => {
-            console.log('Saved clicked', item);
-            this.items.push(item);
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
+
 
 }
